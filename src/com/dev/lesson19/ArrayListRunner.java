bd9e6bc9c242e701447292811a4f2a8c9cbddb55
VladimirImir <mimimvvv@gmail.com>
2023-06-06 23:42:59 +0300

    19.4 - LinkedList.

--- src/com/dev/lesson19/ArrayListRunner.java
+++ src/com/dev/lesson19/ArrayListRunner.java
@@ -1,14 +1,18 @@
 package com.dev.lesson19;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+
+/** 19.4 - LinkedList. */
 
 public class ArrayListRunner {
 
     // [1, 2, 3, 4, 5]
     // [1, 2, 3, 4, 5, 6, null]
     public static void main(String[] args) {
-        List<Integer> integers = new ArrayList<>(5);
+        // List<Integer> integers = new ArrayList<>(5);
+        List<Integer> integers = new LinkedList<>();
         integers.add(1);
         integers.add(2);
         integers.add(3);
