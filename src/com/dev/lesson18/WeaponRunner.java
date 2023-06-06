14db06b333b96cf5ef9440fe698e7b0b964f606f
VladimirImir <mimimvvv@gmail.com>
2023-06-06 20:51:05 +0300

    18.3 - Generics. Теория. Часть 2.

--- src/com/dev/lesson18/WeaponRunner.java
+++ src/com/dev/lesson18/WeaponRunner.java
@@ -1,9 +1,11 @@
 package com.dev.lesson18;
 
 import com.dev.lesson15.Archer;
+import com.dev.lesson15.Hero;
 import com.dev.lesson15.Warrior;
 import com.dev.lesson18.weapon.Bow;
 import com.dev.lesson18.weapon.Sword;
+import com.dev.lesson18.weapon.Weapon;
 
 public class WeaponRunner {
 
@@ -13,5 +15,28 @@ public class WeaponRunner {
 
         Warrior<Sword> warrior = new Warrior<>("Терминатор", 10);
         warrior.setWeapon(new Sword());
+
+        // printWeaponDamage(archer);
+        printWeaponDamage(warrior);
+
+    }
+    /** Параметризация на уровне метода. */
+    /**
+     * Два одининаковых параметрезированный методов написанные по разному.
+     * public static void printWeaponDamage(Hero'<'? extends Weapon> hero)
+     * public static void printWeaponDamage(Hero'<'? super Weapon> hero)
+     * super - ограничения Generic с низу.
+     * public static <T extends Weapon> void printWeaponDamage(Hero<T> hero)
+     */
+
+    /** extends - используется для producers.
+     */
+    /**
+     * super - используется для consumers.
+     */
+
+    public static void printWeaponDamage(Hero<? extends Sword> hero) {
+        Sword weapon = hero.getWeapon();
+        System.out.println(hero.getWeapon().getDamage());
     }
 }
